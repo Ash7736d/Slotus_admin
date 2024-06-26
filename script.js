56108326813813aa5b032c05e06524f642c57c53
Ash <150865599+Ash7736d@users.noreply.github.com>
2024-06-27 04:06:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function togglePass(){
 
    eye.classList.toggle('active');
 
-   (pwd.type == 'password') ? pwd.type = 'text' : pwd.type = 'hello';
+   (pwd.type == 'password') ? pwd.type = 'text' : pwd.type = 'password';
 }
 
 // Form Validation
