f183d46fc2dffc725fb84e50a39a17c688474aad
Ash <150865599+Ash7736d@users.noreply.github.com>
2024-06-27 12:10:05 +0800

    Update script.js

--- page1/script.js
+++ page1/script.js
@@ -2,16 +2,28 @@ document.addEventListener("DOMContentLoaded", function() {
   const texts = ["𝗟𝗼𝗮𝗱𝗶𝗻𝗴 𝗗𝗮𝘁𝗮.", "𝗙𝗲𝘁𝗰𝗵𝗶𝗻𝗴 𝗮𝗹𝗹 𝘂𝘀𝗲𝗿𝘀", "𝗕𝘂𝗶𝗹𝗱𝗶𝗻𝗴 𝗚𝘂𝗶𝗹𝗱 𝗿𝗲𝗾𝘂𝗲𝘀𝘁𝘀"];
   let index = 0;
 
-  function showNextText() {
-    const loadingText = document.getElementById("loading-text");
-    loadingText.textContent = texts[index];
-    loadingText.style.animation = "none";
-    loadingText.offsetHeight; // Trigger reflow
-    loadingText.style.animation = null;
-
-    index = (index + 1) % texts.length;
-    setTimeout(showNextText, 5000); // 3s typing + 2s delay
+  function typeWriter(text, i, fnCallback) {
+    if (i < text.length) {
+      document.getElementById("loading-text").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    } else if (typeof fnCallback == 'function') {
+      setTimeout(fnCallback, 2000);
+    }
   }
 
-  showNextText();
+  function startTextAnimation(i) {
+    if (typeof texts[i] == 'undefined') {
+      setTimeout(function() {
+        startTextAnimation(0);
+      }, 3000);
+    }
+    if (i < texts.length) {
+      typeWriter(texts[i], 0, function() {
+        startTextAnimation(i + 1);
+      });
+    }
+  }
+  startTextAnimation(0);
 });
